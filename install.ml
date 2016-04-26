9faff1207dc450017391e0289e550b8c7f3c1dde
Jeremie Dimino <jdimino@janestreet.com>
2016-04-26 17:03:47 +0100

    113.33.03

---
+++ install.ml
@@ -0,0 +1,11 @@
+#use "topfind";;
+#require "js-build-tools.oasis2opam_install";;
+
+open Oasis2opam_install;;
+
+generate ~package:"ppx_assert"
+  [ oasis_lib "ppx_assert"
+  ; oasis_lib "ppx_assert_lib"
+  ; file "META" ~section:"lib"
+  ; oasis_exe "ppx" ~dest:"../lib/ppx_assert/ppx"
+  ]
