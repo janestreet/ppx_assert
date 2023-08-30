39f705f2566aed0b6c055c824601ec9def9bcd4a
public-release <as-public-release@janestreet.com>
2023-08-30 10:23:16 +0100

    v0.17~preview.128.37+01

--- bench/ppx_assert_bench.ml
+++ bench/ppx_assert_bench.ml
@@ -6,14 +6,9 @@ let () =
   Printexc.register_printer (fun exc ->
     match sexp_of_exn_opt exc with
     | None -> None
-    | Some sexp ->
-      Some (Sexp.to_string_hum ~indent:2 sexp))
+    | Some sexp -> Some (Sexp.to_string_hum ~indent:2 sexp))
+;;
 
-let%bench "test_eq" =
-  [%test_eq: int] 0 0
-
-let%bench "test_pred" =
-  [%test_pred: int] (fun i -> i = 0) 0
-
-let%bench "test_result" =
-  [%test_result: int] ~expect:0 0
+let%bench "test_eq" = [%test_eq: int] 0 0
+let%bench "test_pred" = [%test_pred: int] (fun i -> i = 0) 0
+let%bench "test_result" = [%test_result: int] ~expect:0 0
